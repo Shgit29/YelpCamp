072bdf7a314d59859ae720506ab4368ad7ad9b68
Saad Hassan <shgit29@gmail.com>
2024-02-02 13:02:04 +0500

    added cluster map

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -13,34 +13,44 @@ imageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const campGroundSchema = new Schema({
-  title: String,
-  images: [imageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+const campGroundSchema = new Schema(
+  {
+    title: String,
+    images: [imageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
+    price: Number,
+    description: String,
+    location: String,
 
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+campGroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+<p>${this.description.substring(0, 20)}...</p>
+`;
 });
 
 campGroundSchema.post("findOneAndDelete", async function (doc) {
