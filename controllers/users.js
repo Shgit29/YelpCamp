731523fdbe6f4d54e6296af54409140a6d5a7f67
Saad Hassan <shgit29@gmail.com>
2024-02-14 10:32:40 +0500

    addressed common security issues

--- controllers/users.js
+++ controllers/users.js
@@ -26,18 +26,14 @@ module.exports.renderLogin = (req, res) => {
   res.render("users/login");
 };
 
-module.exports.loginUser = passport.authenticate("local", {
-  failureFlash: true,
-  failureRedirect: "/login",
-}),
-  (req, res) => {
-    req.flash("success", "Welcome back to yelpcamp");
-    console.log(res.locals.returnTo);
-    const redirectUrl = res.locals.returnTo || "/campgrounds";
-    delete req.session.returnTo;
-    console.log(redirectUrl);
-    res.redirect(redirectUrl);
-  };
+module.exports.loginUser = (req, res) => {
+  req.flash("success", "Welcome back to yelpcamp");
+  console.log(res.locals.returnTo);
+  const redirectUrl = res.locals.returnTo || "/campgrounds";
+  // delete req.session.returnTo;
+  console.log(redirectUrl);
+  res.redirect(redirectUrl);
+};
 
 module.exports.logoutUser = (req, res, next) => {
   req.logout(function (err) {
