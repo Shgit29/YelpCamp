d197a9b96c1000d7872f9e4a1e88b660eb9c36cb
Saad Hassan <shgit29@gmail.com>
2024-08-04 00:22:59 +0300

    PORT

--- app.js
+++ app.js
@@ -159,6 +159,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("serving on port 3000...");
 });
